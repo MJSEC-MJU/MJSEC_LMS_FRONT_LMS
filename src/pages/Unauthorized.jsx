f3d346233a4c14c4c4d7edd48fba85e9ca38d5cc
최윤호 <gdool88@gmail.com>
2025-08-23 12:41:57 +0900

    에러페이지 만들긴했는데 수정할듯

--- src/pages/Unauthorized.jsx
+++ src/pages/Unauthorized.jsx
@@ -6,7 +6,7 @@ export default function Unauthorized() {
     <section className="message">
       <div className="box">
         <p>401 Unauthorized</p>
-        <h3>관리자 권한이 없습니다!</h3>
+        <h3>권한이 없습니다!</h3>
       </div>
     </section>
   );
